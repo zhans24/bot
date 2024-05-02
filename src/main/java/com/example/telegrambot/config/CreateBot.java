d0db21253d4384996eac7850ae3e4bd08c32307b
zhanserik <zhasikgaziz@gmail.com>
2024-05-02 12:53:26 +0500

    bot

--- src/main/java/com/example/telegrambot/config/CreateBot.java
+++ src/main/java/com/example/telegrambot/config/CreateBot.java
@@ -4,10 +4,11 @@ import com.example.telegrambot.service.BotService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
-
+@Component
 public class CreateBot {
     @Autowired
     BotService botService;
--- src/main/java/com/example/telegrambot/service/BotService.java
+++ src/main/java/com/example/telegrambot/service/BotService.java
@@ -1,10 +1,8 @@
 package com.example.telegrambot.service;
 
 import com.example.telegrambot.config.Bot;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
